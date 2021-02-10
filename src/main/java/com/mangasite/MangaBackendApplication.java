e8b08aeda4c126165a2f126795d4219a9adf4cee
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-02-09 22:53:58 -0600

    add rsocket security

--- src/main/java/com/mangasite/MangaBackendApplication.java
+++ src/main/java/com/mangasite/MangaBackendApplication.java
@@ -2,10 +2,11 @@ package com.mangasite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.rsocket.RSocketSecurityAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @EnableReactiveMongoRepositories
-@SpringBootApplication
+@SpringBootApplication(exclude = {RSocketSecurityAutoConfiguration.class})
 public class MangaBackendApplication {
 
   public static void main(String[] args) {
