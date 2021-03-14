f2f5be9ec574fa02212b79caf555139c94abd5c5
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-03-14 15:40:36 -0500

    now uses metadata to make request to client

--- src/main/java/com/mangasite/services/ConnectService.java
+++ src/main/java/com/mangasite/services/ConnectService.java
@@ -4,6 +4,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.http.MediaType;
 import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Service;
 import com.mangasite.domain.DeviceInfo;
@@ -69,8 +70,8 @@ public class ConnectService {
   public void watchUserStream(RSocketRequester rSocketRequester, String clientName) {
 
     rSocketRequester
-        .route("")
-        .data(new ServerMessage("Yo Client, tell me what you're reading"))
+        .metadata(
+            new ServerMessage("Yo Client, tell me what you're reading"), MediaType.APPLICATION_JSON)
         .retrieveFlux(String.class)
         .subscribe(
             n -> System.out.println(CLIENT + clientName + " Is Currently Viewing: " + n), ex -> {});
@@ -85,8 +86,7 @@ public class ConnectService {
   public void requestDeviceInfo(RSocketRequester rSocketRequester, String clientName) {
 
     rSocketRequester
-        .route("")
-        .data(new ServerMessage("What device is this?"))
+        .metadata(new ServerMessage("What device is this?"), MediaType.APPLICATION_JSON)
         .retrieveMono(DeviceInfo.class)
         .subscribe(n -> System.out.println(CLIENT + clientName + " Is using " + n));
   }
