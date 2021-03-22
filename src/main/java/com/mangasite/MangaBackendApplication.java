d6403607848743815250f256471ba57e4accdc20
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-03-21 23:02:24 -0500

    exclude unneeded autoconfig

--- src/main/java/com/mangasite/MangaBackendApplication.java
+++ src/main/java/com/mangasite/MangaBackendApplication.java
@@ -1,18 +1,47 @@
 package com.mangasite;
 
 import static org.springframework.fu.jafu.Jafu.reactiveWebApplication;
-import static org.springframework.fu.jafu.webflux.WebFluxServerDsl.webFlux;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.aop.AopAutoConfiguration;
+import org.springframework.boot.autoconfigure.availability.ApplicationAvailabilityAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveUserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
+import org.springframework.boot.autoconfigure.task.TaskSchedulingAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.client.RestTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.ClientHttpConnectorAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 import org.springframework.fu.jafu.JafuApplication;
+import com.mangasite.config.init.RegistrarDeferredInitializer;
 
 @EnableReactiveMongoRepositories
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+      JmxAutoConfiguration.class,
+      AopAutoConfiguration.class,
+      CacheAutoConfiguration.class,
+      WebClientAutoConfiguration.class,
+      RestTemplateAutoConfiguration.class,
+      MessageSourceAutoConfiguration.class,
+      TaskExecutionAutoConfiguration.class,
+      TaskSchedulingAutoConfiguration.class,
+      UserDetailsServiceAutoConfiguration.class,
+      ClientHttpConnectorAutoConfiguration.class,
+      ApplicationAvailabilityAutoConfiguration.class,
+      SpringApplicationAdminJmxAutoConfiguration.class,
+      ReactiveUserDetailsServiceAutoConfiguration.class,
+    })
 public class MangaBackendApplication {
 
   public static void main(String[] args) {
 
-    JafuApplication app = reactiveWebApplication(dsl -> dsl.enable(webFlux()));
+    JafuApplication app =
+        reactiveWebApplication(dsl -> dsl.enable(new RegistrarDeferredInitializer()));
 
     app.run(args);
   }
