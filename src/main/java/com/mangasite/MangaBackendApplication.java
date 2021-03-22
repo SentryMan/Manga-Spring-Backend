6fe507609516dd1a70ef2488d44f3b9034d8c8f3
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-03-21 19:25:00 -0500

    set up Jafu

--- src/main/java/com/mangasite/MangaBackendApplication.java
+++ src/main/java/com/mangasite/MangaBackendApplication.java
@@ -1,14 +1,19 @@
 package com.mangasite;
 
-import org.springframework.boot.SpringApplication;
+import static org.springframework.fu.jafu.Jafu.reactiveWebApplication;
+import static org.springframework.fu.jafu.webflux.WebFluxServerDsl.webFlux;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
+import org.springframework.fu.jafu.JafuApplication;
 
 @EnableReactiveMongoRepositories
 @SpringBootApplication
 public class MangaBackendApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(MangaBackendApplication.class, args);
+
+    JafuApplication app = reactiveWebApplication(dsl -> dsl.enable(webFlux()));
+
+    app.run(args);
   }
 }
