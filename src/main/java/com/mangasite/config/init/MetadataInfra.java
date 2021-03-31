47c48adb4488f454c42a5a1c74427fefb4ca95d8
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-03-31 18:56:09 -0500

    Revert "Let's Try Without JaFu"
    This reverts commit f22eaa056bb9882db25446d75c8e0a3b1363d97b.

---
+++ src/main/java/com/mangasite/config/init/MetadataInfra.java
@@ -0,0 +1,29 @@
+package com.mangasite.config.init;
+
+import java.util.Collection;
+import java.util.Set;
+import org.springframework.beans.factory.support.BeanNameGenerator;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReaderFactory;
+import org.springframework.init.func.FunctionalInstallerImportRegistrars;
+import org.springframework.init.func.ImportRegistrars;
+import org.springframework.init.func.InfrastructureProvider;
+
+public class MetadataInfra implements InfrastructureProvider {
+
+  @Override
+  public Collection<? extends ApplicationContextInitializer<GenericApplicationContext>>
+      getInitializers(GenericApplicationContext context) {
+
+    return Set.of(
+        ac ->
+            ac.registerBean(
+                MetadataReaderFactory.class,
+                () -> new CachingMetadataReaderFactory(ac.getBeanFactory().getBeanClassLoader())),
+        ac -> ac.registerBean(BeanNameGenerator.class, AnnotationBeanNameGenerator::new),
+        ac -> ac.registerBean(ImportRegistrars.class, FunctionalInstallerImportRegistrars::new));
+  }
+}
