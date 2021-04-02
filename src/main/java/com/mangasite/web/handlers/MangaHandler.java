90d32b7cf2b2de2c18d02282fe6fbdc2fa454865
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-04-02 14:20:57 -0500

    fix date conversion

--- src/main/java/com/mangasite/web/handlers/MangaHandler.java
+++ src/main/java/com/mangasite/web/handlers/MangaHandler.java
@@ -1,15 +1,14 @@
 package com.mangasite.web.handlers;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.chrono.ChronoZonedDateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import com.mangasite.domain.requests.MangaChangeRequest;
 import com.mangasite.services.MangaService;
-import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
 
 @Component
@@ -26,22 +25,14 @@ public class MangaHandler {
   }
 
   public Mono<ServerResponse> patchUpdateDate(ServerRequest request) {
-    var formatter = new SimpleDateFormat("yyyy-mm-dd");
-    var dateOp =
+    var epochSecondOp =
         request
             .queryParam("updateDate")
-            .map(
-                s -> {
-                  try {
-                    return formatter.parse(s).toInstant();
-                  } catch (ParseException e) {
-                    throw Exceptions.propagate(e);
-                  }
-                })
-            .map(Instant::toEpochMilli);
+            .map(s -> LocalDate.parse(s).atStartOfDay(ZoneId.systemDefault()))
+            .map(ChronoZonedDateTime::toEpochSecond);
     return service
-        .updateLD(Integer.parseInt(request.pathVariable("id")), dateOp)
+        .updateLD(Integer.parseInt(request.pathVariable("id")), epochSecondOp)
         .flatMap(ServerResponse.ok()::bodyValue)
-        .switchIfEmpty(ServerResponse.badRequest().bodyValue("Manga Doesn't Exists"));
+        .switchIfEmpty(ServerResponse.badRequest().bodyValue("Manga Doesn't Exist"));
   }
 }
