118349aa8cabdf3dc05b41e9ee3205f0e825a4bd
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-05-13 18:51:17 -0500

    404 errors and RSocket Advice

---
+++ src/main/java/com/mangasite/domain/ControllerAdviceWrapper.java
@@ -0,0 +1,33 @@
+package com.mangasite.domain;
+
+import org.springframework.messaging.handler.MessagingAdviceBean;
+import org.springframework.web.method.ControllerAdviceBean;
+
+public class ControllerAdviceWrapper implements MessagingAdviceBean {
+  private final ControllerAdviceBean delegate;
+
+  public ControllerAdviceWrapper(ControllerAdviceBean delegate) {
+    this.delegate = delegate;
+  }
+
+  @Override
+  public int getOrder() {
+    return delegate.getOrder();
+  }
+
+  @Override
+  public Class<?> getBeanType() {
+    return delegate.getBeanType();
+  }
+
+  @Override
+  public Object resolveBean() {
+    return delegate.resolveBean();
+  }
+
+  @Override
+  public boolean isApplicableToBeanType(Class<?> beanType) {
+    return delegate.isApplicableToBeanType(beanType);
+  }
+  // delegate all methods
+}
---
+++ src/main/java/com/mangasite/rsocket/RSocketAdvice.java
@@ -0,0 +1,22 @@
+package com.mangasite.rsocket;
+
+import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
+import org.springframework.messaging.handler.invocation.MethodArgumentResolutionException;
+import io.rsocket.exceptions.CustomRSocketException;
+import io.rsocket.exceptions.RejectedException;
+import reactor.core.publisher.Mono;
+
+public class RSocketAdvice {
+
+  @MessageExceptionHandler
+  public Mono<RejectedException> invalidPayload(MethodArgumentResolutionException marex) {
+
+    return Mono.error(new RejectedException("Invalid Payload", marex));
+  }
+
+  @MessageExceptionHandler
+  public Mono<CustomRSocketException> customRsocketEX(CustomRSocketException ex) {
+
+    return Mono.error(ex);
+  }
+}
