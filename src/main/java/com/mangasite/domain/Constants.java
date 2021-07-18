27a4036d892c6ae78b263139d1088af561b0427a
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2021-07-18 18:42:42 -0500

    class to interface where applicable

--- src/main/java/com/mangasite/domain/Constants.java
+++ src/main/java/com/mangasite/domain/Constants.java
@@ -7,15 +7,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 import org.springframework.data.mongodb.core.ChangeStreamOptions;
 import org.springframework.messaging.rsocket.RSocketRequester;
 
-public class Constants {
-  private Constants() {}
+public interface Constants {
 
-  public static final String ADMIN = "ADMIN";
-  public static final String USER = "USER";
-  public static final ChangeStreamOptions FULL_DOC =
-      ChangeStreamOptions.builder().returnFullDocumentOnUpdate().build();
+  String ADMIN = "ADMIN";
+  String USER = "USER";
+  ChangeStreamOptions FULL_DOC = ChangeStreamOptions.builder().returnFullDocumentOnUpdate().build();
 
-  public static final AtomicInteger ACTIVE_CONNECTIONS = new AtomicInteger();
-  public static final Map<String, RSocketRequester> CLIENT_REQUESTER_MAP = new HashMap<>();
-  public static final Map<String, String> CLIENT_MANGA_MAP = new HashMap<>();
+  AtomicInteger ACTIVE_CONNECTIONS = new AtomicInteger();
+  Map<String, RSocketRequester> CLIENT_REQUESTER_MAP = new HashMap<>();
+  Map<String, String> CLIENT_MANGA_MAP = new HashMap<>();
 }
--- src/main/java/com/mangasite/domain/init/AppInitializer.java
+++ src/main/java/com/mangasite/domain/init/AppInitializer.java
@@ -4,13 +4,12 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.support.GenericApplicationContext;
 
 import com.mangasite.filters.SPAWebFilter;
-import com.mangasite.services.ConnectService;
 
 public class AppInitializer implements ApplicationContextInitializer<GenericApplicationContext> {
 
   @Override
   public void initialize(GenericApplicationContext context) {
-    context.registerBean(ConnectService.class, ConnectService::new);
     context.registerBean(SPAWebFilter.class, SPAWebFilter::new);
+    RSocketServerInitializer.initialize(context);
   }
 }
