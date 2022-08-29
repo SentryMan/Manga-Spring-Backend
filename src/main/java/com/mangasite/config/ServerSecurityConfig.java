7676e54824d5c8fa6c5cb4219e9331ebebd0d403
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-08-28 21:29:20 -0400

    fly.io

--- src/main/java/com/mangasite/config/ServerSecurityConfig.java
+++ src/main/java/com/mangasite/config/ServerSecurityConfig.java
@@ -105,7 +105,11 @@ public class ServerSecurityConfig {
     corsConfig.applyPermitDefaultValues();
     corsConfig.addAllowedMethod(HttpMethod.GET);
     corsConfig.setAllowedOrigins(
-        List.of("http://www.manga-spring.com", "http://localhost:4200", "http://localhost:8080"));
+        List.of(
+            "http://www.manga-spring.com",
+            "http://localhost:4200",
+            "http://localhost:8080",
+            "https://manga-spring.fly.dev"));
 
     final var source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", corsConfig);
