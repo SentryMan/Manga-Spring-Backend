78ea9b63835fbddeb70c57543e06346d3c4e99a0
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-08-31 18:24:44 -0400

    Update ServerSecurityConfig.java

--- src/main/java/com/mangasite/config/ServerSecurityConfig.java
+++ src/main/java/com/mangasite/config/ServerSecurityConfig.java
@@ -107,6 +107,7 @@ public class ServerSecurityConfig {
     corsConfig.setAllowedOriginPatterns(
         List.of(
             "http://www.manga-spring.com",
+            "https://www.manga-spring.com",
             "http://localhost:4200",
             "http://localhost:8080",
             "https://manga-spring--*-firebase-*.web.app",
