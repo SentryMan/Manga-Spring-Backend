5079d5513297c1f6bc7d5c5f8130fc535836a739
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-09-29 22:03:24 -0400

    Update RouterConfig.java

--- src/main/java/com/mangasite/web/routers/RouterConfig.java
+++ src/main/java/com/mangasite/web/routers/RouterConfig.java
@@ -3,25 +3,27 @@ package com.mangasite.web.routers;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
-import java.net.URI;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.mangasite.rsocket.RSocketChapterController;
 import com.mangasite.services.TokenService;
 
+import reactor.core.publisher.Mono;
+
 @Configuration
-public class TokenRouter {
+public class RouterConfig {
 
   @Bean
-  RouterFunction<ServerResponse> tokenRoute(TokenService service) {
+  RouterFunction<ServerResponse> tokenRoute(TokenService service, RSocketChapterController chaps) {
     return route(GET("api/getToken"), service::getToken)
         .andRoute(
-            GET("api/getToken").negate(),
+            GET("api/chapter/update/{id}"),
             req ->
-                ServerResponse.permanentRedirect(URI.create("https://manga-spring.com/404"))
-                    .build());
+                Mono.fromRunnable(
+                    () ->
+                        chaps.updateClients(req.pathVariable("id").transform(Integer::parseInt))));
   }
 }
