aaf44eafa74d65039a280b073e17e78913258960
Josiah Noel <32279667+SentryMan@users.noreply.github.com>
2022-10-23 19:25:15 -0400

    better token library and exclude some dependencies

--- src/main/java/com/mangasite/services/TokenService.java
+++ src/main/java/com/mangasite/services/TokenService.java
@@ -1,11 +1,12 @@
 package com.mangasite.services;
 
 import static com.mangasite.security.AppRole.ANYONE;
-import static io.jsonwebtoken.SignatureAlgorithm.HS256;
 
 import java.security.Principal;
-import java.util.Date;
-import java.util.HashMap;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Base64;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
 
@@ -15,37 +16,55 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.mangasite.security.AppRole;
 
 import io.avaje.config.Config;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
 import io.rsocket.exceptions.RejectedSetupException;
 import reactor.core.publisher.Mono;
 
 public class TokenService {
   private TokenService() {}
 
-  private static final int VALIDITY = Config.getInt("jwt.validity", 1800);
+  private static final long VALIDITY = Config.getLong("jwt.validity", 1800);
+  private static final String issuer = Config.get("jwt.issuer", "memes");
+  private static final Algorithm algorithm =
+      Config.get("jwt.secret.key", "secretkey").transform(Algorithm::HMAC256);
 
-  private static final String KEY = Config.get("jwt.secret.key", "secretkey");
-
-  private static final JwtParser parser = KEY.transform(Jwts.parser()::setSigningKey);
+  private static final JWTVerifier verifier =
+      JWT.require(algorithm)
+          .withIssuer(issuer)
+          .withClaim(
+              "role",
+              (c, d) -> {
+                final var decodedPayload = new String(Base64.getDecoder().decode(d.getPayload()));
+                for (final var r : AppRole.values()) {
+                  if (decodedPayload.contains(r.getPassword())
+                      && decodedPayload.contains(r.getUsername())) {
+                    return true;
+                  }
+                }
+                return false;
+              })
+          .build();
 
   public static Mono<ServerResponse> getToken(ServerRequest request) {
+
     return request
         .principal()
         .map(Principal::getName)
-        .map(AppRole::getRole)
+        .map(AppRole::getByUserName)
         .map(
             role ->
-                Jwts.builder()
-                    .setClaims(HashMap.newHashMap(1))
-                    .setSubject(role.getUsername())
-                    .setIssuedAt(new Date(System.currentTimeMillis()))
-                    .setExpiration(new Date(System.currentTimeMillis() + VALIDITY * 100))
-                    .signWith(HS256, KEY)
-                    .compact())
+                JWT.create()
+                    .withIssuedAt(Instant.now())
+                    .withExpiresAt(Instant.now().plus(VALIDITY, ChronoUnit.SECONDS))
+                    .withClaim("role", role.name())
+                    .withIssuer(issuer)
+                    .withPayload(Map.of(role.getUsername(), role.getPassword()))
+                    .sign(algorithm))
         .flatMap(ServerResponse.ok()::bodyValue);
   }
 
@@ -54,11 +73,11 @@ public class TokenService {
     return authentication
         .getCredentials()
         .toString()
-        .transform(parser::parseClaimsJws)
-        .getBody()
-        .getSubject()
+        .transform(verifier::verify)
+        .getClaim("role")
+        .asString()
         .transform(Mono::just)
-        .map(AppRole::getRole)
+        .map(AppRole::getByRole)
         .filter(Predicate.not(ANYONE::equals))
         .switchIfEmpty(Mono.error(new RejectedSetupException("User Doesn't Exist")))
         .map(
